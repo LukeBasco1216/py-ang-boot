0b971620d6522dc9129965650cffde154859ac09
LukeBasco1216 <basco.luke@iisgalvanimi.edu.it>
2022-11-03 19:02:02 +0000

    idk

--- primo/BTS15.1/src/app/foo/foo.component.ts
+++ primo/BTS15.1/src/app/foo/foo.component.ts
@@ -11,7 +11,24 @@ export class FooComponent implements OnInit {
   loading!: boolean;
   o!:Observable<Object>;
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
+  makeRequest(): void {
+    //Notifichiamo che stiamo attendendo dei dati
+    this.loading = true; 
+    //Facciamo una get e otteniamo l'oggetto Observable che attende la risposta
+    this.o = this.http.get('https://jsonplaceholder.typicode.com/posts/1');
+    //Attacchiamo all'Observable o un metodo "observer" che verrà lanciato quando arriva la 
+    //risposta
+    this.o.subscribe(this.getData);
+  }
+  //Il metodo che notifica la risposta (nota che usiamo una "arrow function")
+  getData = (d : Object) =>
+  {
+    this.data = d; //Notifico l’oggetto ricevuto dal server
+    this.loading = false;  // Notifico che ho ricevuto i dati
+  }
+
+
 
   ngOnInit(): void {
   }
