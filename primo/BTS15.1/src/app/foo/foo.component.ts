ca13fe8ae3f7e79d7c0dc03cc61b22800d94d3e0
LukeBasco1216 <basco.luke@iisgalvanimi.edu.it>
2022-11-03 13:53:38 +0000

    idk

---
+++ primo/BTS15.1/src/app/foo/foo.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-foo',
+  templateUrl: './foo.component.html',
+  styleUrls: ['./foo.component.css']
+})
+export class FooComponent implements OnInit {
+
+  data = Object;
+  loading = boolean;
+  o = Observable<Object>;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
