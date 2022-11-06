27ef24d7dbfbff269844f532640440de4ebb4a94
LukeBasco1216 <basco.luke@iisgalvanimi.edu.it>
2022-11-06 19:45:04 +0000

    ok

--- primo/BTS15.1/src/app/foo/foo.component.ts
+++ primo/BTS15.1/src/app/foo/foo.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+  // scaricare questo: npm install --save rxjs-compat 
 @Component({
   selector: 'app-foo',
   templateUrl: './foo.component.html',
@@ -7,25 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooComponent implements OnInit {
   
-  data!: Object;
-  loading!: boolean;
-  o!:Observable<Object>;
+  data: Object;
+  loading: boolean;
+  o :Observable<Object>;
 
-  constructor(public http: HttpClient) { }
-  makeRequest(): void {
-    //Notifichiamo che stiamo attendendo dei dati
-    this.loading = true; 
-    //Facciamo una get e otteniamo l'oggetto Observable che attende la risposta
-    this.o = this.http.get('https://jsonplaceholder.typicode.com/posts/1');
-    //Attacchiamo all'Observable o un metodo "observer" che verrà lanciato quando arriva la 
-    //risposta
-    this.o.subscribe(this.getData);
-  }
-  //Il metodo che notifica la risposta (nota che usiamo una "arrow function")
-  getData = (d : Object) =>
-  {
-    this.data = d; //Notifico l’oggetto ricevuto dal server
-    this.loading = false;  // Notifico che ho ricevuto i dati
+  //http: HttpClient;
+  //constructor(http: HttpClient) { 
+  //  this.http = http;
+  //}
+
+  constructor(public http: HttpClient) {}
+  
+  //Nota bene, questo è un metodo alternativo e compatto per fare la stessa cosa di 
+  //makeRequest senza dichiarare la variabile Observable e creando l’arrow function   
+  //direttamente dentro il metodo subscribe
+  makeCompactRequest(): void {
+    this.loading = true;
+    this.http
+      .get('https://jsonplaceholder.typicode.com/posts/1')
+      .subscribe(newData => {
+      this.data = newData;
+      this.loading = false;
+      });
+    }
+  
+  //L'operazione di post necessita un parametro in più.
+  //Viene creata una stringa (JSON.strigify) a partire da un oggetto Typescript
+  makeCompactPost(): void {
+    this.loading = true;
+    this.http
+      .post('https://jsonplaceholder.typicode.com/posts',
+        JSON.stringify({ 
+          body: 'bar',
+          title: 'foo',
+          userId: 1
+        })
+      )
+      .subscribe(data => {
+        this.data = data;
+        this.loading = false;
+      });
   }
 
 
